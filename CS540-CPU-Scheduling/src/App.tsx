44da72ac2f2ab04e2b9b91b3431992d580cb0e72
phoozies <tpvo.personal@gmail.comgit config --global user.name>
2025-03-06 08:41:30 -0600

    Added grid div container styling to charts

--- CS540-CPU-Scheduling/src/App.tsx
+++ CS540-CPU-Scheduling/src/App.tsx
@@ -4,7 +4,7 @@ import GanttChart from "./components/GanttChart";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-    const [results, setResults] = useState<any[]>([]);
+    const [results, setResults] = useState<{ algorithm: string; result: any[] }[]>([]);
 
     // Define a list of colors for the charts
     const colors = [
@@ -23,28 +23,44 @@ function App() {
             <Scheduler setResults={setResults} />
 
             {/* Gantt Chart Section */}
-            <div className="row mt-4">
-                <div className="col">
-                    <h4>Execution Timeline (Gantt Chart)</h4>
-                    {results.map((res, index) => (
-                        <div key={index} className="mb-4">
-                            <h5>{res.algorithm}</h5>
-                            <GanttChart
-                                algorithm={res.algorithm}
-                                result={res.result}
-                                color={colors[index % colors.length]} // Assign a unique color
-                            />
-                        </div>
-                    ))}
-                </div>
+            <div className="mt-4">
+                <h4>Execution Timeline (Gantt Chart)</h4>
+
+                {results.length > 0 && (
+                    <>
+                        {results.reduce<{ algorithm: string; result: any[] }[][]>((rows, res, index) => {
+                            if (index % 2 === 0) {
+                                // Start a new row every 2 charts
+                                rows.push([res]);
+                            } else {
+                                // Add to the current row
+                                rows[rows.length - 1].push(res);
+                            }
+                            return rows;
+                        }, []).map((row: { algorithm: string; result: any[] }[], rowIndex: number) => (
+                            <div className="row justify-content-center mt-3" key={rowIndex}>
+                                {row.map((res: { algorithm: string; result: any[] }, index: number) => (
+                                    <div key={index} className="col-md-6 mb-4">
+                                        <h5>{res.algorithm}</h5>
+                                        <GanttChart
+                                            algorithm={res.algorithm}
+                                            result={res.result}
+                                            color={colors[(rowIndex * 2 + index) % colors.length]} // Assign a unique color
+                                        />
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
+                    </>
+                )}
             </div>
 
             {/* Results Section */}
-            <div className="row mt-4">
-                <div className="col">
-                    <h4>Results:</h4>
-                    {results.map((res, index) => (
-                        <div key={index} className="mt-3 p-2 border rounded bg-light">
+            <div className="mt-4">
+                <h4>Results:</h4>
+                <div className="row justify-content-center">
+                    {results.map((res: { algorithm: string; result: any[] }, index: number) => (
+                        <div key={index} className="col-md-6 mt-3 p-2 border rounded bg-light">
                             <h5>{res.algorithm}</h5>
                             <pre>{JSON.stringify(res.result, null, 2)}</pre>
                         </div>
